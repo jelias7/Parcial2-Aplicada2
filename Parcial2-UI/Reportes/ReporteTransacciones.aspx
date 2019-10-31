898baf5e2995e48ef4aa6115ab67b84e3460a587
Juan Rosario <juanelias18@hotmail.com>
2019-10-30 20:31:16 -0400

    Reporte hecho

---
+++ Parcial2-UI/Reportes/ReporteTransacciones.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReporteTransacciones.aspx.cs" Inherits="Parcial2_UI.Reportes.ReporteTransacciones" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        html,body,form,#div {
+            height: 100%; 
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="div">
+          <rsweb:ReportViewer ID="MyReportViewer" runat="server"  ProcessingMode="Remote" Height="100%" Width="100%">
+          <ServerReport ReportServerUrl="" ReportPath="" />
+          </rsweb:ReportViewer>
+         </div>
+    </form>
+</body>
+</html>
---
+++ Parcial2-UI/Reportes/ReporteTransacciones.aspx.cs
@@ -0,0 +1,31 @@
+﻿using BLL;
+using Entidades;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Parcial2_UI.Reportes
+{
+    public partial class ReporteTransacciones : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                RepositorioBase<Transacciones> repositorio = new RepositorioBase<Transacciones>();
+                var lista = repositorio.GetList(x => true);
+
+                MyReportViewer.ProcessingMode = ProcessingMode.Local;
+                MyReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\Reportes\ListadoTransacciones.rdlc");
+
+                MyReportViewer.LocalReport.DataSources.Add(new ReportDataSource("Transacciones", lista));
+                MyReportViewer.LocalReport.Refresh();
+
+            }
+        }
+    }
+}
